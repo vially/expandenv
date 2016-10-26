decbc323c3fd6a79da04c09690cf29e81e27e96d
Valentin Hăloiu <vially.ichb@gmail.com>
2016-10-26 21:53:48 +0300

    Add base64 encoding option

--- README.md
+++ README.md
@@ -3,14 +3,14 @@
 Replace environment variables from input file
 
 ## Usage
-`expandenv FILE`
+`expandenv [--base64] FILE`
 
 ## Installation
 
 Grab a binary from [Releases](https://github.com/vially/expandenv/releases) or build from source:
 `go get github.com/vially/expandenv`
 
-## Example
+## Basic Example
 
 ```bash
 $ cat input.txt
@@ -19,4 +19,26 @@ $ export GREETER_NAME=John
 $ expandenv input.txt > output.txt
 $ cat output.txt
 Hello John
+```
+
+## Base64 Encode Example
+
+```bash
+$ cat secrets.yaml
+apiVersion: v1
+kind: Secret
+metadata:
+  name: mysecret
+type: Opaque
+data:
+  password: $PASSWORD
+$ export PASSWORD=s3cret
+$ expandenv --base64 secrets.yaml
+apiVersion: v1
+kind: Secret
+metadata:
+  name: mysecret
+type: Opaque
+data:
+  password: czNjcmV0
 ```
--- main.go
+++ main.go
@@ -1,34 +1,44 @@
 package main
 
 import (
+	"encoding/base64"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
 
-	"github.com/codegangsta/cli"
+	"github.com/jawher/mow.cli"
 )
 
 func main() {
-	app := cli.NewApp()
-	app.Name = "expandenv"
-	app.Usage = "Expand environment variables in input file"
-	app.Version = "0.0.1"
-	app.Author = "Valentin Hăloiu"
-	app.Email = "vially.ichb@gmail.com"
-	app.Action = func(c *cli.Context) {
-		if len(c.Args()) == 0 {
-			log.SetFlags(0)
-			log.Fatal("Usage: expandenv FILE")
-		}
+	app := cli.App("expandenv", "Expand environment variables in input file")
+	app.Spec = "[-b] FILE"
+	app.Version("v version", "0.0.2")
 
-		buf, err := ioutil.ReadFile(c.Args()[0])
+	var (
+		encode = app.BoolOpt("b base64", false, "base64 encode values")
+		src    = app.StringArg("FILE", "", "input file")
+	)
+
+	app.Action = func() {
+		buf, err := ioutil.ReadFile(*src)
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		fmt.Print(os.ExpandEnv(string(buf)))
+		fileData := string(buf)
+		var mapper func(string) string
+		if !*encode {
+			mapper = os.Getenv
+		} else {
+			mapper = envBase64Encoder
+		}
+		fmt.Print(os.Expand(fileData, mapper))
 	}
 
 	app.Run(os.Args)
+}
+
+func envBase64Encoder(key string) string {
+	return base64.StdEncoding.EncodeToString([]byte(os.Getenv(key)))
 }
